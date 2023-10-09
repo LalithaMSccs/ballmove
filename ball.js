c82b80e0bf5fc83c3c0369573ef34272748e485b
LalithaMSccs <140373845+LalithaMSccs@users.noreply.github.com>
2023-10-09 18:47:35 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,12 @@
+
+ var positionY=0;
+ const ball=document.getElementById("ball");
+
+function moveball(){
+  
+   ball.style.top= positionY+'px';
+   ball.style.left=positionY+'px';
+   positionY++;
+     
+}
+setInterval(moveball,100);
